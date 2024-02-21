ae1d69450c0b0bf90f533e70b78db161488cf249
Kyrylo Sovailo <k.sovailo@gmail.com>
2024-02-21 01:11:58 +0100

    Parallelized

--- include/optalg/neighborhood.hpp
+++ include/optalg/neighborhood.hpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <limits>
 #include <vector>
+#include <thread>
 #include <time.h>
 
 namespace opt
@@ -28,8 +29,17 @@ namespace opt
         //Define types
         typedef typename Problem::Solution Solution;
         typedef typename Problem::SolutionContainer Container;
+        struct Thread
+        {
+            typename Container::const_iterator begin, end;
+            const Solution *solution;
+            double heuristic;
+            std::thread thread;
+        };
         
-        //Start clock
+        //Start clock and threads
+        const unsigned int nthreads = std::thread::hardware_concurrency();
+        std::vector<Thread> threads(nthreads);
         const bool clock_limited = std::isfinite(time_max);
         const clock_t clock_max = clock_limited ? static_cast<clock_t>(time_max * CLOCKS_PER_SEC) : 0;
         const clock_t start = clock();
@@ -45,31 +55,53 @@ namespace opt
             //Get neighborhood
             Container neighbors = problem.neighbors(solution);
             
-            //Search best neighbor
-            auto best_neighbor = neighbors.cend();
-            double best_neighbor_heuristic = std::numeric_limits<double>::infinity();
-            for (auto neighbor = neighbors.cbegin(); neighbor != neighbors.cend(); neighbor++)
+            //Search best neighbors
+            for (unsigned int id = 0; id < threads.size(); id++)
             {
-                double neighbor_heuristic = problem.heuristic(*neighbor, iter);
-                if (neighbor_heuristic < solution_heuristic && neighbor_heuristic < best_neighbor_heuristic)
+                //TODO: generalize not only for random iterators
+                threads[id].begin = neighbors.begin() + (neighbors.size() * id / nthreads);
+                threads[id].end = neighbors.begin() + (neighbors.size() * (id + 1) / nthreads);
+                threads[id].solution = nullptr;
+                threads[id].heuristic = std::numeric_limits<double>::infinity();
+                threads[id].thread = std::thread(
+                [iter, solution_heuristic, &problem](Thread *thread)
                 {
-                    best_neighbor = neighbor;
-                    best_neighbor_heuristic = neighbor_heuristic;
+                    for (auto neighbor = thread->begin; neighbor != thread->end; neighbor++)
+                    {
+                        double neighbor_heuristic = problem.heuristic(*neighbor, iter);
+                        if (neighbor_heuristic < solution_heuristic && neighbor_heuristic < thread->heuristic)
+                        {
+                            thread->solution = &(*neighbor);
+                            thread->heuristic = neighbor_heuristic;
+                        }
+                    }
+                }, &threads[id]);
+            }
+
+            //Search best neighbor
+            const Solution *best_neighbor = nullptr;
+            double best_neighbor_heuristic = std::numeric_limits<double>::infinity();
+            for (unsigned int id = 0; id < threads.size(); id++)
+            {
+                threads[id].thread.join();
+                if (threads[id].heuristic < solution_heuristic && threads[id].heuristic < best_neighbor_heuristic)
+                {
+                    best_neighbor = threads[id].solution;
+                    best_neighbor_heuristic = threads[id].heuristic;
                 }
             }
-            
+
             //Go to best neighbor
-            if (best_neighbor != neighbors.cend())
+            if (best_neighbor != nullptr)
             {
                 solution = *best_neighbor;
-                solution_heuristic = best_neighbor_heuristic;
                 if (log != nullptr) log->push_back(solution);
             }
             
             //Exit
             if (problem.good(solution))
             {
-                if (best_neighbor == neighbors.cend()) break;                   //No better neighbor
+                if (best_neighbor == nullptr) break;                            //No better neighbor
                 else if (iter >= iter_max) break;                               //Maximum iteration reached
                 else if (clock_limited && clock() - start >= clock_max) break;  //Maximum time reached
             }
